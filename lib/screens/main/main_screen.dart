86870399bdd6e1243ed2e49a5800393a105de2a5
Hoseok Seong <shsshs0125@naver.com>
2024-05-02 18:59:10 +0900

    메인 페이지 레이아웃 개발

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -9,42 +9,91 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white70,
         body: SafeArea(
           child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: <Widget>[
-                  // const SizedBox(height: 50),
-                  // const Text(
-                  //   "Puppicasso",
-                  //   style: TextStyle(
-                  //     fontSize: 32,
-                  //     color: Colors.blueAccent,
-                  //     fontWeight: FontWeight.bold,
-                  //   ),
-                  // ),
-                  // const SizedBox(height: 12),
-                  // const Text(
-                  //   "Puppicasso, 내 반려견만을 위한 AI 프로필",
-                  //   style: TextStyle(
-                  //     fontSize: 15,
-                  //     color: Colors.white,
-                  //     fontWeight: FontWeight.bold,
-                  //   ),
-                  //   textAlign: TextAlign.center,
-                  // ),
-                  // const Spacer(flex: 1),
-                  // ElevatedButton(
-                  //   onPressed: () {
-                  //     Navigator.pushNamed(context, "/profile_create");
-                  //   },
-                  //   child: Text("프로필 사진 만들기"),
-                  // ),
-                  // const SizedBox(height: 48),
-                ],
-              ),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Puppicasso는 반려견을 위한 다양한 컨셉의 AI 프로필 사진을 제공하는 서비스입니다",
+                        style: TextStyle(
+                            fontSize: 16,
+                            height: 1.8
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/picture_create");
+                        },
+                        child: Text("지금 바로 사진 생성하기"),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Column(
+                          children: [
+                            SizedBox(height: 14),
+                            Text(
+                              "회원 등급",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  height: 1.8
+                              ),
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              "사용 중인 이용권",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  height: 1.8
+                              ),
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              "생성한 사진 수",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  height: 1.8
+                              ),
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              "남은 생성 횟수",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  height: 1.8
+                              ),
+                            ),
+                            SizedBox(height: 14),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
+        ),
     );
   }
 }
