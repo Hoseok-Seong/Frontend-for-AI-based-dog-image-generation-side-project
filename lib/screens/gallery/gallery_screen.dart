908ef8a1d5d403257ba78fc37762523fb334f974
Hoseok Seong <shsshs0125@naver.com>
2024-05-04 08:12:01 +0900

    gallery screen

---
+++ lib/screens/gallery/gallery_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class GalleryScreen extends StatelessWidget {
+  static String routeName = "/gallery";
+
+  const GalleryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+
+          ),
+        ),
+      ),
+    );
+  }
+}
