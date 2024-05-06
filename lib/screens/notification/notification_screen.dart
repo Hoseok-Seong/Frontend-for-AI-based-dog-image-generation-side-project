7ccedbf0c93ce67941ad880aadf89ef0ffb8dbe3
Hoseok Seong <shsshs0125@naver.com>
2024-05-06 21:54:15 +0900

    user_info, notification, subscription

---
+++ lib/screens/notification/notification_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatelessWidget {
+  static String routeName = "/notification";
+
+  const NotificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/subscription/subscription_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SubscriptionScreen extends StatelessWidget {
+  static String routeName = "/subscription";
+
+  const SubscriptionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+
+          ),
+        ),
+      ),
+    );
+  }
+}
