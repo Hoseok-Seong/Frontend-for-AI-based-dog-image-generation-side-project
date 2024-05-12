947aaa8eff97d06caa2057194d4cf2dce4628170
Hoseok Seong <shsshs0125@naver.com>
2024-05-12 23:00:41 +0900

    main screen 디자인 수정

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -23,6 +23,7 @@ class MainScreen extends StatelessWidget {
                   child: Column(
                     children: [
                       const Text(
+                        textAlign: TextAlign.center,
                         "Puppicasso는 반려견을 위한 다양한 컨셉의 AI 프로필 사진을 제공하는 서비스입니다",
                         style: TextStyle(
                             fontSize: 16,
@@ -53,96 +54,144 @@ class MainScreen extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             SizedBox(height: 14),
-                            RichText(
-                              text: TextSpan(
-                                style: DefaultTextStyle.of(context).style,
-                                children: const <TextSpan>[
-                                  TextSpan(
-                                    text: '회원 등급: ',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      height: 1.8,
-                                    ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: const <TextSpan>[
+                                      TextSpan(
+                                        text: '회원 등급:',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          height: 1.8,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  TextSpan(
-                                    text: '테스트',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      height: 1.8,
-                                      color: Colors.blue,
-                                    ),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: const <TextSpan>[
+                                      TextSpan(
+                                        text: '테스트',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          height: 1.8,
+                                          color: Colors.blue,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                             SizedBox(height: 12),
-                            RichText(
-                              text: TextSpan(
-                                style: DefaultTextStyle.of(context).style,
-                                children: const <TextSpan>[
-                                  TextSpan(
-                                    text: '사용 중인 이용권: ',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      height: 1.8,
-                                    ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: const <TextSpan>[
+                                      TextSpan(
+                                        text: '사용 중인 이용권:',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          height: 1.8,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  TextSpan(
-                                    text: '테스트',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      height: 1.8,
-                                      color: Colors.blue,
-                                    ),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: const <TextSpan>[
+                                      TextSpan(
+                                        text: '테스트',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          height: 1.8,
+                                          color: Colors.blue,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                             SizedBox(height: 12),
-                            RichText(
-                              text: TextSpan(
-                                style: DefaultTextStyle.of(context).style,
-                                children: const <TextSpan>[
-                                  TextSpan(
-                                    text: '생성한 사진 수: ',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      height: 1.8,
-                                    ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: const <TextSpan>[
+                                      TextSpan(
+                                        text: '생성한 사진 수: ',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          height: 1.8,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  TextSpan(
-                                    text: '테스트',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      height: 1.8,
-                                      color: Colors.blue,
-                                    ),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: const <TextSpan>[
+                                      TextSpan(
+                                        text: '테스트',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          height: 1.8,
+                                          color: Colors.blue,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                             SizedBox(height: 12),
-                            RichText(
-                              text: TextSpan(
-                                style: DefaultTextStyle.of(context).style,
-                                children: const <TextSpan>[
-                                  TextSpan(
-                                    text: '남은 생성 횟수: ',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      height: 1.8,
-                                    ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: const <TextSpan>[
+                                      TextSpan(
+                                        text: '남은 생성 횟수: ',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          height: 1.8,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  TextSpan(
-                                    text: '테스트',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      height: 1.8,
-                                      color: Colors.blue,
-                                    ),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: const <TextSpan>[
+                                      TextSpan(
+                                        text: '테스트',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          height: 1.8,
+                                          color: Colors.blue,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                             SizedBox(height: 14),
                           ],
