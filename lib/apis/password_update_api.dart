3d7d9277e93b2feee749802f4bc0f2134495a716
Hoseok Seong <shsshs0125@naver.com>
2024-09-23 23:59:23 +0900

    비밀번호 수정 작업 중

--- lib/apis/password_update_api.dart
+++ lib/apis/password_update_api.dart
@@ -8,14 +8,6 @@ class PasswordUpdateAPI {
 
   Future<bool> passwordUpdate() async {
     try {
-      // final response = await dio.post('/password-update', data: userResetPasswordReq.toJson());
-
-      // if (response.statusCode == 200) {
-      //
-      //   return true;
-      // } else {
-      //   throw Exception("'비밀번호 변경'이 실패하였습니다");
-      // }
       return true;
     } on DioException catch (e) {
       throw Exception("네트워크 오류가 발생했습니다. 다시 시도해주세요.");
